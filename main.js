55fdd78caf980129d4927831dff5668d4f53ec6e
Will <will@willacton.com>
2015-04-24 14:41:57 -0700

    read multiple files

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
 let Markov = require('./markov.js'),
 	fs = require('fs');
 
-module.exports.buildChainFromFiles = function (prefixLength, file) {
-	//let files = Array.prototype.slice.apply(arguments); // create array of filenames from arbitrary arguments
+module.exports.buildChainFromFiles = function (prefixLength) {
 	let chain = Markov.Chain(prefixLength);
+	let files = Array.prototype.slice.call(arguments, 1);
+	let contents = '';
 
-	let contents = fs.readFileSync(file, 'utf8');
+	files.forEach(function (file) {
+		contents += fs.readFileSync(file, 'utf8');
+	});
 
 	chain.Build(contents)
 
--- tests/test_main.js
+++ tests/test_main.js
@@ -4,9 +4,17 @@ let main = require('../main.js'),
 	assert = require('assert');;
 
 (function () {
-	console.log('Testing reading contents from one file...')
+	console.log('Testing reading contents from one file...');
 	let chain = main.buildChainFromFiles(2, './tests/test.txt');
 	console.log('"'+chain.Generate(50)+'"');
 
-	assert(chain.Generate(25).length > 0)
+	assert(chain.Generate(25).length > 0);
+})();
+
+(function () {
+	console.log('Testing reading contents from two files...');
+	let chain = main.buildChainFromFiles(2, './tests/test.txt', './tests/test2.txt');
+	console.log('"'+chain.Generate(100)+'"');
+
+	assert(chain.Generate(25).length > 0);
 })();
