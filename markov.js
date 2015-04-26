9c82fca83e152769ca3023abebeb76688a5cc98a
Will <will@willacton.com>
2015-04-26 14:14:00 -0700

    updated summary in markov.js

--- markov.js
+++ markov.js
@@ -4,9 +4,9 @@
 	Markov chain text generator
 	Author: Will Acton
 	Date: April, 2015
-	Summary: Builds a Markov chain of a words to generate random text
-	Based off of the Go walk-through here:
-	https://golang.org/doc/codewalk/markov/
+	Summary: Builds a Markov chain of words from seed texts, 
+	then generates strings based on the chain. Based off of 
+	the Go code-walk here: https://golang.org/doc/codewalk/markov/
 */
 
 // Prefix definition, for ease of use
